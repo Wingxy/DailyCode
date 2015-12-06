5da419c58e28c1acb8bbc60ebba2e6a138c987a1
Wingxy <wingxy19891017@gmail.com>
2015-12-06 23:04:46 +0800

    创建控制器和自定义cell

---
+++ 02-PhotoCarousel/02-PhotoCarousel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  02-PhotoCarousel
+//
+//  Created by wxy on 15/12/6.
+//  Copyright © 2015年 wxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 02-PhotoCarousel/02-PhotoCarousel/CarouselViewController.h
@@ -0,0 +1,13 @@
+//
+//  CarouselViewController.h
+//  02-PhotoCarousel
+//
+//  Created by wxy on 15/12/6.
+//  Copyright © 2015年 wxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CarouselViewController : UICollectionViewController
+
+@end

c61a532d93b37d89ae28ff588d26461caa303faa
xinyu_wang <xinyu_wang@uyoung.com.cn>
2015-12-19 15:23:11 +0800

    换算公式

--- AutoLayout简单使用-01/AutoLayout简单使用-01/ViewController.m
+++ AutoLayout简单使用-01/AutoLayout简单使用-01/ViewController.m
@@ -79,6 +79,19 @@
     NSLayoutConstraint *bottomBtnEqualHeightConstraint = [NSLayoutConstraint constraintWithItem:bottomBtn attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:topLeftBtn attribute:NSLayoutAttributeHeight multiplier:1.0 constant:0];
     [self.view addConstraint:bottomBtnEqualHeightConstraint];
     
+    /*
+     
+     竖屏时：设三个Button的高度是x未知数，三个Button高度一样，10+x+10+x+10=568 --->x=269，三个按钮高度都是269像素。BottomBtn的宽度是10+y+10=320--->y=300,BottomBtn的宽就是300像素。现在主是计算TopLeftBtn  TopRightBtn的宽了，TopLeftBtn与TopRightBtn的宽相等，那么10+z+10+z+10=320-->z=145,他们的宽就是145像素了，现在一切就好说了
+     
+     TopLeftBtn的frame 就是（10,10,145,269）   TopRightBtn的frame为（10+145+10,10,145,269）
+     
+     BottomBtn的frame就是(10,10+269+10,145,269)
+     
+     横屏一样，只不过是宽度换过来了，其他的一样。
+     
+     如果是4s或者6  6plus只不过是屏幕尺寸不一样而已都按上面的公式计算，得出的frame也不一样，所以说只要你的约束加得好，就会控制控件的位置，实现多屏配适配
+     
+     */
     
 }
 

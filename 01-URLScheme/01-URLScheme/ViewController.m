e5fb530c12443c3fb6629fce622135b265c8b6df
xinyu_wang <xinyu_wang@uyoung.com.cn>
2015-12-26 12:32:35 +0800

    我也不知道这提交什么

--- 01-URLScheme/01-URLScheme/ViewController.m
+++ 01-URLScheme/01-URLScheme/ViewController.m
@@ -46,4 +46,5 @@
     // Dispose of any resources that can be recreated.
 }
 
+
 @end
--- UmengShareDemo/UmengShareDemo/ViewController.m
+++ UmengShareDemo/UmengShareDemo/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "UMSocial.h"
 
-@interface ViewController ()
+@interface ViewController ()<UMSocialUIDelegate>
 
 @end
 
@@ -17,11 +18,43 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    }
+
+
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+//    //注意：分享到微信好友、微信朋友圈、微信收藏、QQ空间、QQ好友、来往好友、来往朋友圈、易信好友、易信朋友圈、Facebook、Twitter、Instagram等平台需要参考各自的集成方法
+//    [UMSocialSnsService presentSnsIconSheetView:self
+//                                         appKey:@"567a0e18e0f55a037a00ae8d"
+//                                      shareText:@"你好，我是友盟"
+//                                     shareImage:[UIImage imageNamed:@"ad_01"]
+//                                shareToSnsNames:[NSArray arrayWithObjects:UMShareToSina,UMShareToWechatSession,UMShareToQQ,UMShareToDouban,UMShareToFacebook,UMShareToLWTimeline,UMShareToRenren,UMShareToAlipaySession,UMShareToEmail,UMShareToFlickr,UMShareToKakaoTalk,UMShareToSms,nil]
+//                                       delegate:self];
+    
+    
+//    //注意：分享到微信好友、微信朋友圈、微信收藏、QQ空间、QQ好友、来往好友、来往朋友圈、易信好友、易信朋友圈、Facebook、Twitter、Instagram等平台需要参考各自的集成方法
+//    [[UMSocialControllerService defaultControllerService] setShareText:@"分享内嵌文字" shareImage:[UIImage imageNamed:@"icon"] socialUIDelegate:self];        //设置分享内容和回调对象
+//    [UMSocialSnsPlatformManager getSocialPlatformWithName:UMShareToDouban].snsClickHandler(self,[UMSocialControllerService defaultControllerService],YES);
+    
+//    [[UMSocialDataService defaultDataService]  postSNSWithTypes:@[UMShareToSina] content:@"我在迪拜" image:[UIImage imageNamed:@"ad_01"] location:nil urlResource:nil presentedController:self completion:^(UMSocialResponseEntity *response){
+//        if (response.responseCode == UMSResponseCodeSuccess) {
+//            NSLog(@"分享成功！");
+//        }
+//    }];
+
+    
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)didFinishGetUMSocialDataInViewController:(UMSocialResponseEntity *)response
+{
+    //根据`responseCode`得到发送结果,如果分享成功
+    if(response.responseCode == UMSResponseCodeSuccess)
+    {
+        //得到分享到的微博平台名
+        NSLog(@"share to sns name is %@",[[response.data allKeys] objectAtIndex:0]);
+    }
 }
-
 @end
